8a156ab54e5cd9bd7ecb105bad2a009a4355ef49
AlejandroMasin <masinalejandro828@gmail.com>
2023-08-31 06:13:45 -0300

    controllers itineraries

--- src/controllers/itinerariesController.js
+++ src/controllers/itinerariesController.js
@@ -1,5 +1,5 @@
-const Itinerary = require("../models/Itinerary");
 const City = require("../models/City");
+const Itinerary = require("../models/Itinerary");
 
 const getItineraries = async (req, res) => {
 
@@ -31,20 +31,24 @@ const getItinerary = async (req, res) => {
 }
 
 const getItineraryByCity = async (req, res) => {
-
     try {
-        const { id } = req.query
+        const { id } = req.query;
 
-        let itinerariesEncontrados = await Itinerary.find(City._id == id)
+        // Verificar si se proporcionó un ID válido
+        if (!id) {
+            return res.status(400).json({ message: "ID de ciudad no proporcionado en la consulta." });
+        }
 
-        console.log(id);
-        res.status(200).json(itinerariesEncontrados)
+        // Utilizar el ID proporcionado para filtrar los itinerarios por ciudad
+        let itinerariesEncontrados = await Itinerary.find({ _ciudad: id });
 
+        res.status(200).json({itinerariesEncontrados});
     } catch (err) {
-        res.status(500).json({ message: err })
+        res.status(500).json({ message: err.message });
     }
-
 }
+
+
 
 const addItinerary = async (req, res) => {
 
@@ -67,7 +71,7 @@ const addItinerary = async (req, res) => {
         //     _ciudad: cityFound
         // })
 
-        await cityFound.updateOne({itineraries:[...cityFound.itineraries, newItinerary]})
+        await cityFound.updateOne({ itineraries: [...cityFound.itineraries, newItinerary] })
 
         let cityFoundUpdated = await City.findById(id)
 
@@ -80,11 +84,56 @@ const addItinerary = async (req, res) => {
         res.status(500).json({ message: e.message });
     }
 
-} 
+}
+
+const updateItinerary = async (req, res) => {
+    try {
+        let newData = {
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            duracion: req.body.duracion,
+            likes: req.body.likes,
+            hashtags: req.body.hashtags
+        }
+
+        const { id } = req.params;
+        await Itinerary.findByIdAndUpdate(id, newData);
+
+        res.status(200).json({
+            "message": "Itinerary has been updated",
+            newData
+        });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const deleteItinerary = async (req, res) => {
+
+
+    try {
+        let { id } = req.query
+
+
+        const deletedItinerary = await Itinerary.deleteOne({ _id: id })
+
+        res.status(201).json({
+            "message": "Itinerary has been deleted",
+            "deletedItinerary": deletedItinerary
+        })
+    } catch (err) {
+        res.status(500).json({ message: err })
+    }
+
+}
 
 module.exports = {
     addItinerary,
     getItineraries,
     getItinerary,
-    getItineraryByCity
+    getItineraryByCity,
+    updateItinerary,
+    deleteItinerary
 }
--- src/router/router.js
+++ src/router/router.js
@@ -3,17 +3,20 @@ const express = require("express")
 const router = express.Router()
 
 const {getCities, getCity, addCity, deleteCity, updateCity} = require("../controllers/citiesController")
-const { getItineraries, getItinerary, getItineraryByCity, addItinerary } = require("../controllers/itinerariesController")
+const { getItineraries, getItinerary, getItineraryByCity, addItinerary, updateItinerary, deleteItinerary } = require("../controllers/itinerariesController")
 
 router.get("/cities", getCities)
 router.get("/city/:id", getCity)
 router.post("/cities", addCity)
-router.delete("/cities", deleteCity)
 router.put("/city/:id", updateCity)
+router.delete("/cities", deleteCity)
 
 router.get("/itineraries", getItineraries)
-router.get("/itineraries/:id", getItineraryByCity)
+router.get('/itinerariesbycity', getItineraryByCity);
 router.get("/itinerary/:id", getItinerary)
 router.post("/itineraries", addItinerary)
+router.put("/itinerary/:id", updateItinerary)
+router.delete("/itinerary", deleteItinerary)
+
 
 module.exports = router
